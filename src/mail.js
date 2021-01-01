71bb9757bb4d98691642d9f4cf1233219cc5460d
Markee Smith <markee.j.smith@gmail.com>
2021-01-01 14:19:45 -0500

    Edited some of the mail transport options

--- src/mail.js
+++ src/mail.js
@@ -3,7 +3,8 @@ const nodemailer = require('nodemailer');
 const transport = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
-  secure: true,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS
